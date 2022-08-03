f5c9a7fa227c7797d3908b431cb8001c42b3a848
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-03 16:23:05 +0900

    setting color-button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByRole('link', {name: /learn react/i});
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  render(<App/>);
+
+  // find and element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({
+    backgroundColor: 'red'
+  });
+
+  // click Button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+
+  expect(colorButton).toHaveStyle({
+    backgroundColor: 'blue'
+  });
+
+  // expect to button text to be 'Change to 'red'
+  expect(colorButton.textContent).toBe('Change to red');
+});
+
+test('initial conditions', () => {
+  render(<App/>);
+
+  
+  // check that the button starts out enalbed
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+
+  // check that the checkbox start s out unchecke
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+      <input type="checkbox" />
     </div>
   );
 }
