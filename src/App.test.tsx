4cfdd1d8d4d5fa8bc26d87a915ac04f967363012
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-08 22:16:10 +0900

    set eslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeDisabled();
 });
