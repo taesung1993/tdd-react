fee435662020c37bad8ec223882392021696d3b8
taesung1993 <cheonyulin@gmail.com>
2022-08-27 20:34:39 +0900

    ready to do final test

---
+++ src/tests/orderPhase.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+test('order pahses for happy path', () => {
+  // render app
+  // add ice cream scoops and toppings
+  // find and click order button
+  // check summary information based on order
+  // accept terms and conditions click button to confirm order
+  // confirm order number on confirmation page
+  // click new order button on confirmation page
+  // check that scoops and toppings subtotals have been reset
+  // do we need to await anything to avoid test errors?
+});
